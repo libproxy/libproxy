0fc29c82c6940a90c499e707642e958159c0d398
npmccallum <npmccallum@c587cffe-e639-0410-9787-d7902ae8ed56>
2007-10-17 16:08:23 +0000

    rename gconf plugin to 'gnome'; make it session aware

--- trunk/src/plugins/gnome.c
+++ trunk/src/plugins/gnome.c
@@ -4,13 +4,9 @@
 #include <misc.h>
 #include <proxy_factory.h>
 
+#include <gdk/gdk.h>
+#include <gdk/gdkx.h>
 #include <gconf/gconf-client.h>
-
-static bool
-in_gnome_session()
-{
-	return true;
-}
 
 pxConfig *
 gconf_config_cb(pxProxyFactory *self)
@@ -86,15 +82,43 @@ gconf_config_cb(pxProxyFactory *self)
 void
 gconf_on_get_proxy(pxProxyFactory *self)
 {
-	if (in_gnome_session())
-		px_proxy_factory_config_add(self, "gconf", PX_CONFIG_CATEGORY_SESSION, gconf_config_cb);
-	else
-		px_proxy_factory_config_del(self, "gconf");
+	int argc = 0;
+	GdkScreen *screen = NULL;
+	const char *wm = NULL;
+	
+	// Connect to X
+	if (!gdk_init_check(&argc, NULL))  goto not_gnome_session;
+	
+	// Get the default screen
+	screen = gdk_screen_get_default();
+	if (!screen)                       goto not_gnome_session;
+	
+	// Get the window manager of that screen
+	wm = gdk_x11_screen_get_window_manager_name(screen);
+	if (!wm || strcmp(wm, "Metacity")) goto not_gnome_session;
+	
+	// Close the connection to X, so we don't crash if disconnected
+	gdk_display_close(gdk_screen_get_display(screen));
+	
+	// Make sure this config is registered
+	px_proxy_factory_config_add(self, "gnome", PX_CONFIG_CATEGORY_SESSION, gconf_config_cb);
+	return;
+	
+	// We aren't in a gnome session, so make sure this is config is NOT registered
+	not_gnome_session:
+		if (screen)
+			gdk_display_close(gdk_screen_get_display(screen));
+		px_proxy_factory_config_del(self, "gnome");
 }
 
 bool
 on_proxy_factory_instantiate(pxProxyFactory *self)
 {
+	// Note that we instantiate like this because SESSION config plugins
+	// are only suppossed to remain registered while the application
+	// is actually IN that session.  So for instance, if the app
+	// was run in GNU screen and is taken out of the GNOME sesion
+	// it was started in, we should handle that gracefully.
 	g_type_init();
 	px_proxy_factory_on_get_proxy_add(self, gconf_on_get_proxy);
 	return true;
@@ -104,5 +128,5 @@ void
 on_proxy_factory_destantiate(pxProxyFactory *self)
 {
 	px_proxy_factory_on_get_proxy_del(self, gconf_on_get_proxy);
-	px_proxy_factory_config_del(self, "gconf");
+	px_proxy_factory_config_del(self, "gnome");
 }
