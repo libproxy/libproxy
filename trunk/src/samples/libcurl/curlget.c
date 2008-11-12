4eb4106e1640bbdbbe4138e9bb674c7e42958c85
dominique.leuenberger <dominique.leuenberger@c587cffe-e639-0410-9787-d7902ae8ed56>
2008-11-12 20:19:04 +0000

    Adjust the copyright header of the libcurl sample program

--- trunk/src/samples/libcurl/curlget.c
+++ trunk/src/samples/libcurl/curlget.c
@@ -1,8 +1,22 @@
-/*
-   Copyright header
-   Demo pogram using libcurl to fetch the data, injecting proxy
-   information received from libproxy.
-*/
+/*******************************************************************************
+ * libproxy - A library for proxy configuration
+ * Copyright (C) 2006 Nathaniel McCallum <nathaniel@natemccallum.com>
+ * Copyright (C) 2008 Dominique Leuenberger <dominique-libproxy@leuenberger.net>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ ******************************************************************************/
 
 #include "proxy.h"
 #include "curl/curl.h"
