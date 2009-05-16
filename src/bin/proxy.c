d4535c8aac50d9fe434abbbd47a1fb6a66660c89
dominique.leuenberger <dominique.leuenberger@c587cffe-e639-0410-9787-d7902ae8ed56>
2009-05-16 18:01:52 +0000

    Fix build for BSD. Same as we do in other source files

--- src/bin/proxy.c
+++ src/bin/proxy.c
@@ -16,7 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  ******************************************************************************/
-#define _GNU_SOURCE
+#define _BSD_SOURCE
 
 #include <stdio.h>
 #include <stdlib.h>
