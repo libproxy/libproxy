d789b0c4bc555dd53baca6afcbf6bc9e52c545df
dominique.leuenberger <dominique.leuenberger@c587cffe-e639-0410-9787-d7902ae8ed56>
2009-05-21 00:01:50 +0000

    .NET: a very simple, very basic test program in C# (tested with mono 2.0

---
+++ src/samples/dotnet/proxy.cs
@@ -0,0 +1,18 @@
+using System;
+using libproxy;
+
+public class proxy {
+    public static int Main (string[] args)
+    {
+        if (args.Length > 0) {
+            ProxyFactory pf = new ProxyFactory();
+            string[] Proxies = pf.GetProxies(args[0]);
+            Console.WriteLine(Proxies[0]);
+            return 0;
+        } else {
+            Console.WriteLine("Please start the program with one parameter");
+            Console.WriteLine("Example: proxy.exe http://libproxy.googlecode.com/");
+            return 1;
+        }
+    }
+}
