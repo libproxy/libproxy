d08554abdb4b4067373e51ed2356bcdfa6c89553
dominique.leuenberger <dominique.leuenberger@c587cffe-e639-0410-9787-d7902ae8ed56>
2009-09-29 17:26:53 +0000

    testsuite: simple test app to verify ini parsers function.

---
+++ trunk/src/bin/iniparse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <strdict.h>
+#include <config_file.h>
+
+int main() {
+  pxConfigFile* cf = px_config_file_new("test.d/test_libproxy_config_file.conf");
+  if (!cf) {
+    printf("Could not find conf file.\n.");
+    return 1;
+  }
+  char* val1 = px_config_file_get_value(cf, "SECTION", "inikey1");
+  char* val2 = px_config_file_get_value(cf, "SECTION", "inikey2");
+  printf("val1 -> '%s' / val2 -> '%s'\n", val1 == NULL ? "NULL" : val1, val2 == NULL ? "NULL" : val2);
+  px_config_file_free(cf);
+  return 0;
+}
