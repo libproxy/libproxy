fa714d435b9de2919887c3ebbb3e185dfac5d389
dominique.leuenberger <dominique.leuenberger@c587cffe-e639-0410-9787-d7902ae8ed56>
2009-09-29 17:45:32 +0000

    test: iniparser should also check the __DEFAULT__ Section.

--- trunk/src/bin/iniparse.c
+++ trunk/src/bin/iniparse.c
@@ -8,8 +8,8 @@ int main() {
     printf("Could not find conf file.\n.");
     return 1;
   }
-  char* val1 = px_config_file_get_value(cf, "SECTION", "inikey1");
-  char* val2 = px_config_file_get_value(cf, "SECTION", "inikey2");
+  char* val1 = px_config_file_get_value(cf, "__DEFAULT__", "key1");
+  char* val2 = px_config_file_get_value(cf, "SECTION", "key2");
   printf("val1 -> '%s' / val2 -> '%s'\n", val1 == NULL ? "NULL" : val1, val2 == NULL ? "NULL" : val2);
   px_config_file_free(cf);
   return 0;
