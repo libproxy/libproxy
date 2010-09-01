cefe1c5b9a2141fb22abf193652e1b4f6eb702ff
nicolas.dufresne <nicolas.dufresne@c587cffe-e639-0410-9787-d7902ae8ed56>
2010-09-01 20:34:55 +0000

    Wrong revision for 0.4.6

--- tags/libproxy-0.4.6/bindings/csharp/libproxy-sharp-1.0.pc.cmake
+++
@@ -1,5 +0,0 @@
-
-Name: Libproxy#
-Description: Libproxy# - .NET Binding to libproxy
-Version: 0.4.5
-Libs: -r:@assembly@
--- tags/libproxy-0.4.6/bindings/ruby/test.rb
+++
@@ -1,8 +0,0 @@
-require 'Libproxy'
-
-pf = Libproxy.px_proxy_factory_new
-
-proxies = Libproxy.px_proxy_factory_get_proxies(pf, "http://www.google.com")
-
-print proxies
-
