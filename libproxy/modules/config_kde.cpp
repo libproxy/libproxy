dda0418a917136ed16930a556d158565231867b2
Fabian Vogt <fvogt@suse.com>
2016-05-10 10:25:39 +0200

    config_kde: Remove use of C++11
    - Use std::map::find instead of map::at (issue #29)
    - Add additional safety check
    - Return as const ref

--- libproxy/modules/config_kde.cpp
+++ libproxy/modules/config_kde.cpp
@@ -156,22 +156,24 @@ private:
     }
 
     // Neither key nor def must contain '
-    string kde_config_val(const string &key, const string &def) throw (runtime_error) {
+    const string &kde_config_val(const string &key, const string &def) throw (runtime_error) {
         if (cache_needs_refresh())
             cache.clear();
-        else
-            try {
-                // Already in cache?
-                return cache.at(key);
-            } catch(...) {} // Not in cache
+        else {
+            // Already in cache?
+            map<string, string>::iterator it = cache.find(key);
+            if(it != cache.end())
+                return it->second;
+        }
 
-        string result = command_output(
+        // Although all values are specified internally and/or validated,
+        // checking is better than trusting.
+        if(key.find('\'') != string::npos || def.find('\'') != string::npos)
+            return def;
+
+        // Add result to cache and return it
+        return cache[key] = command_output(
                 command + " --file kioslaverc --group 'Proxy Settings' --key '" + key + "' --default '" + def + "'");
-
-        // Add result to cache
-        cache[key] = result;
-
-        return result;
     }
 
     // Used for cache invalidation
