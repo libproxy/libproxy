b25b32a8fbcb1ce85f84ea4709c17d63cf9c8c99
Laurent Bigonville <bigon@bigon.be>
2020-12-14 16:15:43 +0100

    python: Fix the loading of the module
    argtype must be "sequence of C data types" according to the
    documentation.
    
    Also use the correct type, px_proxy_factory_get_proxies and
    px_proxy_factory_free_proxies use char**
    
    Call px_proxy_factory_free_proxies on the object returned by
    px_proxy_factory_get_proxies and not the array
    
    Fixes: #125

--- bindings/python/libproxy.py
+++ bindings/python/libproxy.py
@@ -39,8 +39,8 @@ def _load(name, *versions):
 _libproxy = _load("proxy", 1)
 _libproxy.px_proxy_factory_new.restype = ctypes.POINTER(ctypes.c_void_p)
 _libproxy.px_proxy_factory_free.argtypes = ctypes.c_void_p,
-_libproxy.px_proxy_factory_get_proxies.restype = ctypes.POINTER(ctypes.c_void_p)
-_libproxy.px_proxy_factory_free_proxies.argtypes = ctypes.POINTER(ctypes.c_void_p)
+_libproxy.px_proxy_factory_get_proxies.restype = ctypes.POINTER(ctypes.c_char_p)
+_libproxy.px_proxy_factory_free_proxies.argtypes = [ctypes.POINTER(ctypes.c_char_p)]
 
 class ProxyFactory(object):
     """A ProxyFactory object is used to provide potential proxies to use
@@ -135,7 +135,7 @@ class ProxyFactory(object):
                 proxies.append(proxy_bytes.decode('utf-8', errors='replace'))
             i += 1
 
-        _libproxy.px_proxy_factory_free_proxies(proxies)
+        _libproxy.px_proxy_factory_free_proxies(array)
         
         return proxies
         
