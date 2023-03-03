597408052666438d5a0c8f70f5c5eb37eb6f8694
Jan-Michael Brummer <jan.brummer@tabos.org>
2023-03-03 17:40:12 +0100

    Move tests a level higher (#58)

--- tests/px-manager-helper.c
+++ tests/px-manager-helper.c
@@ -25,7 +25,7 @@
 PxManager *
 px_test_manager_new (const char *config_plugin)
 {
-  g_autofree char *path = g_strdup_printf ("%s/../backend/plugins", g_getenv ("G_TEST_BUILDDIR"));
+  g_autofree char *path = g_test_build_filename (G_TEST_BUILT, "../src/backend/plugins", NULL);
 
   return g_object_new (PX_TYPE_MANAGER,
                        "plugins-dir", path,
