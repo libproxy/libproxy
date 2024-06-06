ff720e5b95befbb2d3004db5fea96e37c8ceb527
Jan-Michael Brummer <jan-michael.brummer1@volkswagen.de>
2024-06-06 06:11:18 +0200

    Handle empty ignore settings
    Fixes: https://github.com/libproxy/libproxy/issues/299

--- src/backend/px-manager.c
+++ src/backend/px-manager.c
@@ -716,7 +716,7 @@ ignore_domain (GUri *uri,
   if (g_strcmp0 (ignore, "*") == 0)
     return TRUE;
 
-  if (!host)
+  if (!host || !ignore)
     return FALSE;
 
   ignore_split = g_strsplit (ignore, ":", -1);
